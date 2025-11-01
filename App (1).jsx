6a13954bb40b869e1f620b5889223aac95e5147f
mythic-lilith <23BIA50007@CUCHD.IN>
2025-11-01 18:19:21 +0530

    Add files via upload

---
+++ App (1).jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import LibraryManagement from "./components/LibraryManagement";
+
+function App() {
+  return (
+    <div style={{ backgroundColor: "black", minHeight: "100vh", padding: "20px" }}>
+      <LibraryManagement />
+      Hi there! Welcome to Experiment 7.2
+    </div>
+
+   
+  );
+}
+
+export default App;
---
+++ vite.config.mjs
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+  base: './',
+  root: '.',
+  publicDir: 'public',
+  build: {
+    rollupOptions: {
+      input: 'public/index.html',
+    },
+    outDir: 'dist',
+  },
+});
